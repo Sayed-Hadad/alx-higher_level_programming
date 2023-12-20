481e2d710022cccf8b35040e8fc22ce0c42cf9e0
sayed elhadad <anasesozaking@gmail.com>
2023-12-20 15:14:36 +0200

    Qick Foxx

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,7 @@ def safe_print_division(a, b):
         res = a / b
         return res
     except ZeroDivisionError:
-        return None
+        res = None
     finally:
         print("Inside result: {}".format(res))
+    return res
